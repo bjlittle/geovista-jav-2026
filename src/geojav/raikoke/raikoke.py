c1c7dbaf0a0cca6fc146ff18cf5dd2ad49817562
Bill Little <bill.james.little@gmail.com>
2026-05-24 22:34:36 +0100

    fix deprecation warning (#101)

--- src/geojav/raikoke/raikoke.py
+++ src/geojav/raikoke/raikoke.py
@@ -508,8 +508,8 @@ def callback_render(value) -> None:
         p.remove_actor("plume")
     else:
         if reset_clip:
-            if p.plane_widgets:
-                p.plane_widgets.pop().Off()
+            if p.widgets.plane_widgets:
+                p.widgets.plane_widgets.pop().Off()
 
         if show_clip:
             xyz = np.asarray(frame.center)
@@ -541,8 +541,8 @@ def callback_render(value) -> None:
             tshow_edges = show_edges
             show_scalar_bar = False
 
-            if p.plane_widgets:
-                p.plane_widgets.pop().Off()
+            if p.widgets.plane_widgets:
+                p.widgets.plane_widgets.pop().Off()
                 p.remove_actor("plume")
 
             if show_smooth:
